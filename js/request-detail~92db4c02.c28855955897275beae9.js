1cce38bd789881751b45126c770432f9bd553276
Travis CI User <travis@example.org>
2020-07-28 06:12:26 +0000

    Merge pull request #331 from rvsia/updateApprovalPageTable
    Change buttons in approval table to be in a row as buttons

--- js/request-detail~92db4c02.c28855955897275beae9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{825:function(e,t,s){"use strict";s.r(t);var a=s(88),r=s.n(a),u=s(0),n=s.n(u),i=s(52),c=s(744),d=s(780),o=s(264),q=s(771),p=s(71);t.default=function(){var e=Object(o.a)(),t=Object(c.a)(["request"]),s=r()(t,1)[0].request,a=[{title:e.formatMessage(q.a.myRequests),to:i.a.requests.index,id:"requests"},{title:e.formatMessage(p.a.requestTitle,{id:s}),id:s}];return n.a.createElement(d.a,{requestBreadcrumbs:a,indexpath:i.a.request})}}}]);
-//# sourceMappingURL=../sourcemaps/request-detail~92db4c02.js.map
