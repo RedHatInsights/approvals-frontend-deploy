88a4a2fe14089eb29c367d5f51480b390c168276
Travis CI User <travis@example.org>
2020-07-29 10:25:15 +0000

    Merge pull request #333 from rvsia/refWizardToModal
    Change add approval process wizard to modal

--- js/all-request-detail~b4e568ab.2b7b73200d16c3879ad9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{830:function(e,t,a){"use strict";a.r(t);var s=a(89),r=a.n(s),u=a(0),n=a.n(u),i=a(52),l=a(751),c=a(783),d=a(265),o=a(773),q=a(70);t.default=function(){var e=Object(d.a)(),t=Object(l.a)(["request"]),a=r()(t,1)[0].request,s=[{title:e.formatMessage(o.a.allRequests),to:i.a.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.a.requestTitle,{id:a}),id:a}];return n.a.createElement(c.a,{indexpath:i.a.allrequest,requestBreadcrumbs:s})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail~b4e568ab.js.map
