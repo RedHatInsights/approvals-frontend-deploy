55a1b0f3eab127d8037876c1bbda16fe8b0ee614
Travis CI User <travis@example.org>
2020-08-04 12:44:06 +0000

    Merge pull request #340 from rvsia/disabledOnPristine
    Disable form submits on pristine in add/edit forms

--- js/request-detail~92db4c02.6dfaf90dea3d3bd0eb7a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{793:function(e,t,s){"use strict";s.r(t);var a=s(87),r=s.n(a),u=s(0),n=s.n(u),i=s(52),c=s(723),d=s(758),o=s(264),q=s(745),p=s(70);t.default=function(){var e=Object(o.a)(),t=Object(c.a)(["request"]),s=r()(t,1)[0].request,a=[{title:e.formatMessage(q.a.myRequests),to:i.a.requests.index,id:"requests"},{title:e.formatMessage(p.a.requestTitle,{id:s}),id:s}];return n.a.createElement(d.a,{requestBreadcrumbs:a,indexpath:i.a.request})}}}]);
-//# sourceMappingURL=../sourcemaps/request-detail~92db4c02.js.map
