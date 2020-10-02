b66c0abbe3ad30338c8456d91fa91cd46d48f010
Travis CI User <travis@example.org>
2020-10-02 06:56:26 +0000

    Merge pull request #390 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-utilities-2.2.4

--- js/request-detail~92db4c02.77fc266aebb6965485ee.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{819:function(e,t,s){"use strict";s.r(t);var a=s(92),r=s.n(a),u=s(0),n=s.n(u),i=s(55),c=s(748),d=s(783),o=s(284),q=s(765),p=s(74);t.default=function(){var e=Object(o.a)(),t=Object(c.a)(["request"]),s=r()(t,1)[0].request,a=[{title:e.formatMessage(q.a.myRequests),to:i.a.requests.index,id:"requests"},{title:e.formatMessage(p.a.requestTitle,{id:s}),id:s}];return n.a.createElement(d.a,{requestBreadcrumbs:a,indexpath:i.a.request})}}}]);
-//# sourceMappingURL=../sourcemaps/request-detail~92db4c02.js.map
