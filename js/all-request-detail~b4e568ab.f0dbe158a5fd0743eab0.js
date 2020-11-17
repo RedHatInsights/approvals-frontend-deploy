db44a717efda5372d9db9951f8ce90d9aea8bcba
Travis CI User <travis@example.org>
2020-11-17 15:48:56 +0000

    Merge pull request #420 from RedHatInsights/master
    [Prod-beta] Sprint 65 master to prod-beta hotfixes

--- js/all-request-detail~b4e568ab.f0dbe158a5fd0743eab0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{805:function(e,t,a){"use strict";a.r(t);var s=a(102),r=a.n(s),u=a(0),n=a.n(u),i=a(65),l=a(678),c=a(737),d=a(285),o=a(696),q=a(82);t.default=function(){var e=Object(d.a)(),t=Object(l.a)(["request"]),a=r()(t,1)[0].request,s=[{title:e.formatMessage(o.a.allRequests),to:i.a.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.a.requestTitle,{id:a}),id:a}];return n.a.createElement(c.a,{indexpath:i.a.allrequest,requestBreadcrumbs:s})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail~b4e568ab.js.map
