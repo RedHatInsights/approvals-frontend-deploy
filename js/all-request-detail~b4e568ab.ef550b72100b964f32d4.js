39c5c522b39c73996501e37ced150fe8f876a5d2
Travis CI User <travis@example.org>
2020-12-23 16:11:29 +0000

    Merge pull request #444 from RedHatInsights/prod-beta
    Sprint 67 merge prod-beta into stable

--- js/all-request-detail~b4e568ab.ef550b72100b964f32d4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{820:function(e,t,a){"use strict";a.r(t);var s=a(106),r=a.n(s),u=a(0),n=a.n(u),i=a(66),l=a(692),c=a(752),d=a(291),o=a(710),q=a(85);t.default=function(){var e=Object(d.a)(),t=Object(l.a)(["request"]),a=r()(t,1)[0].request,s=[{title:e.formatMessage(o.a.allRequests),to:i.a.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.a.requestTitle,{id:a}),id:a}];return n.a.createElement(c.a,{indexpath:i.a.allrequest,requestBreadcrumbs:s})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail~b4e568ab.js.map
