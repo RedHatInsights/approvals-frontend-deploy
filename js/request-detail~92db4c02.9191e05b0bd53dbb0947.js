77b9731f7fe554e3c2ccdf2ce7863e026b6f4887
Travis CI User <travis@example.org>
2021-01-28 14:29:16 +0000

    Merge pull request #465 from RedHatInsights/dependabot/npm_and_yarn/patternfly/react-table-4.20.15
    Bump @patternfly/react-table from 4.19.45 to 4.20.15

---
+++ js/request-detail~92db4c02.9191e05b0bd53dbb0947.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{978:function(e,t,s){"use strict";s.r(t);var a=s(158),r=s.n(a),u=s(0),n=s.n(u),i=s(90),c=s(854),d=s(912),o=s(342),q=s(871),p=s(120);t.default=function(){var e=Object(o.a)(),t=Object(c.a)(["request"]),s=r()(t,1)[0].request,a=[{title:e.formatMessage(q.a.myRequests),to:i.a.requests.index,id:"requests"},{title:e.formatMessage(p.a.requestTitle,{id:s}),id:s}];return n.a.createElement(d.a,{requestBreadcrumbs:a,indexpath:i.a.request})}}}]);
+//# sourceMappingURL=../sourcemaps/request-detail~92db4c02.js.map
