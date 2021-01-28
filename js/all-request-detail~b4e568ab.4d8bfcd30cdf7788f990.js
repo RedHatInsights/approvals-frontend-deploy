699fb054346094337004530b945cc71527714410
Travis CI User <travis@example.org>
2021-01-28 14:07:33 +0000

    Merge pull request #464 from RedHatInsights/dependabot/npm_and_yarn/patternfly/react-core-4.90.2
    Bump @patternfly/react-core from 4.84.4 to 4.90.2

--- js/all-request-detail~b4e568ab.4d8bfcd30cdf7788f990.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{876:function(e,t,a){"use strict";a.r(t);var s=a(133),r=a.n(s),u=a(0),n=a.n(u),i=a(73),l=a(751),c=a(809),d=a(306),o=a(768),q=a(105);t.default=function(){var e=Object(d.a)(),t=Object(l.a)(["request"]),a=r()(t,1)[0].request,s=[{title:e.formatMessage(o.a.allRequests),to:i.a.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.a.requestTitle,{id:a}),id:a}];return n.a.createElement(c.a,{indexpath:i.a.allrequest,requestBreadcrumbs:s})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail~b4e568ab.js.map
