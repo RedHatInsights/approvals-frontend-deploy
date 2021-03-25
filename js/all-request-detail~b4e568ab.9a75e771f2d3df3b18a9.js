48550c445d680ccaa192b63d5f2bc1ae6e0b9aa7
Travis CI User <travis@example.org>
2021-03-25 15:25:49 +0000

    Merge pull request #492 from lgalis/update_to_webpack_5
    Update to frontend components 3.x and webpack 5

--- js/all-request-detail~b4e568ab.9a75e771f2d3df3b18a9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{881:function(e,t,a){"use strict";a.r(t);var s=a(106),r=a.n(s),u=a(0),n=a.n(u),i=a(74),l=a(756),c=a(814),d=a(310),o=a(773),q=a(105);t.default=function(){var e=Object(d.a)(),t=Object(l.a)(["request"]),a=r()(t,1)[0].request,s=[{title:e.formatMessage(o.a.allRequests),to:i.a.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.a.requestTitle,{id:a}),id:a}];return n.a.createElement(c.a,{indexpath:i.a.allrequest,requestBreadcrumbs:s})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail~b4e568ab.js.map
