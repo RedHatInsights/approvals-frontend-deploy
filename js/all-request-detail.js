0ebde78df44ba38e030ee67b27a495d30df9792a
Travis CI User <travis@example.org>
2021-06-11 13:14:53 +0000

    Merge pull request #540 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-config-4.1.10
    Bump @redhat-cloud-services/frontend-components-config from 4.1.9 to 4.1.10

--- js/all-request-detail.js
+++ js/all-request-detail.js
@@ -1,2 +1,2 @@
 (self.webpackChunkapproval=self.webpackChunkapproval||[]).push([[444],{2036:(e,t,s)=>{"use strict";s.r(t),s.d(t,{default:()=>Z});var a=s(28481),r=s(30624),l=s.n(r),u=s(50409),i=s(1054),n=s(39748),d=s(86896),p=s(49214),q=s(27121);const Z=function(){var e=(0,d.Z)(),t=(0,i.Z)(["request"]),s=(0,a.Z)(t,1)[0].request,r=[{title:e.formatMessage(p.Z.allRequests),to:u.Z.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.Z.requestTitle,{id:s}),id:s}];return l().createElement(n.Z,{indexpath:u.Z.allrequest,requestBreadcrumbs:r})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail.5fa1f6e33bafb6bf6a6b.js.map
+//# sourceMappingURL=../sourcemaps/all-request-detail.9ba57b7321a9b9f3b4c0.js.map
