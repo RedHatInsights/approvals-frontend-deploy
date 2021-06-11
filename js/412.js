0a882349147925a5548faa28579a9f95fc7a1f4c
Travis CI User <travis@example.org>
2021-06-11 12:48:57 +0000

    Merge pull request #538 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-3.2.5
    Bump @redhat-cloud-services/frontend-components from 3.2.3 to 3.2.5

--- js/412.js
+++ js/412.js
@@ -1,2 +1,2 @@
 (self.webpackChunkapproval=self.webpackChunkapproval||[]).push([[412],{28412:(e,r,n)=>{"use strict";n.r(r),n.d(r,{default:()=>i});var a=n(30624),t=n.n(a),o=n(334),l=n(58126),p=n(80832),c=n(65045),s=n(35240),u=n(45697),f=function(){return t().createElement(l.Provider,{store:(0,p.Z)()},t().createElement(o.BrowserRouter,{basename:(0,s.eb)(window.location.pathname,1)},t().createElement(c.Z,null)))};f.propTypes={logger:n.n(u)().func};const i=f}}]);
-//# sourceMappingURL=../sourcemaps/412.5d9c6901e80feb7f422c.js.map
+//# sourceMappingURL=../sourcemaps/412.5fa1f6e33bafb6bf6a6b.js.map
