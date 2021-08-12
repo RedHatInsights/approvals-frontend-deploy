2b20d8d53f30d1c324a9f31863d9b8dc4b69c48b
Travis CI User <travis@example.org>
2021-08-12 05:50:42 +0000

    Merge pull request #560 from RedHatInsights/dependabot/npm_and_yarn/patternfly/react-core-4.147.0
    Bump @patternfly/react-core from 4.135.0 to 4.147.0

--- js/all-request-detail.js
+++ js/all-request-detail.js
@@ -1,2 +1,2 @@
 (self.webpackChunkapproval=self.webpackChunkapproval||[]).push([[444],{2036:(e,t,s)=>{"use strict";s.r(t),s.d(t,{default:()=>Z});var a=s(28481),r=s(30624),l=s.n(r),u=s(50409),i=s(1054),n=s(39748),d=s(86896),p=s(49214),q=s(27121);const Z=function(){var e=(0,d.Z)(),t=(0,i.Z)(["request"]),s=(0,a.Z)(t,1)[0].request,r=[{title:e.formatMessage(p.Z.allRequests),to:u.Z.allrequests.index,id:"allrequests"},{title:e.formatMessage(q.Z.requestTitle,{id:s}),id:s}];return l().createElement(n.Z,{indexpath:u.Z.allrequest,requestBreadcrumbs:r})}}}]);
-//# sourceMappingURL=../sourcemaps/all-request-detail.849e08000dc5d2bd6320.js.map
+//# sourceMappingURL=../sourcemaps/all-request-detail.54787e0769c258206e49.js.map
